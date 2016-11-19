92736f6623ea08a6297ac6a74947a312b5d680ba
Дмитрий Мурзин <dm1998@list.ru>
2016-11-19 18:49:50 +0300

    update

---
+++ main.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask, render_template, send_from_directory, request, abort, session, redirect
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+mysql = MySQL(app)
+
+@app.route('/')
+def users():
+    cur = mysql.connection.cursor()
+    cur.execute('''SELECT user, host FROM mysql.user''')
+    rv = cur.fetchall()
+    return str(rv)
+
+if __name__ == '__main__':
+    app.run(debug=True)
