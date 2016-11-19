df6af116289ec999a1c0c7ff11a5d6442d756b90
Дмитрий Мурзин <dm1998@list.ru>
2016-11-19 20:57:41 +0300

    update

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ app = Flask("Simple app")
 
 template_dir = 'templates'
 
-@app.route('/test')
+@app.route('/')
 def users():
-    return render_template('index.html')
+    return render_template('index.html', session=session)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
