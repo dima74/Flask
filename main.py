d0a2ce8c8bd2ef717e56d24ea576b48d8612c49c
Дмитрий Мурзин <dm1998@list.ru>
2016-11-20 17:30:42 +0300

    update

--- main.py
+++ main.py
@@ -137,9 +137,9 @@ def chatPage():
     # search_sring = urllib.unquote(unicode(request.args.get('search', "")))
     search_string = request.args.get('search', "")
     # print("search_sring = ", search_string)
-    print(type(search_string))
-    search_string = search_string.encode("utf8")
-    print("search_sring = ", search_string)
+    # print(type(search_string))
+    # search_string = search_string.encode("utf8")
+    # print("search_sring = ", search_string)
 
     messageContentFilter = "AND INSTR(Messages.content, '%s') > 0" % search_string if search_string else ""
     print(0)
