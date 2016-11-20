5e9a879b7fee23e8b6fb02282c55bc29af819ba0
Дмитрий Мурзин <dm1998@list.ru>
2016-11-20 14:51:33 +0300

    update

--- main.py
+++ main.py
@@ -206,4 +206,4 @@ def print_cookie():
 
 if __name__ == '__main__':
     app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
-    app.run(debug=True, host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', threaded=True)
