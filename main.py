86edb9ccd2b6dda6d0afe31a1816cc9f3a4d3724
Дмитрий Мурзин <dm1998@list.ru>
2016-11-20 17:06:56 +0300

    update

--- main.py
+++ main.py
@@ -26,7 +26,8 @@ template_dir = 'templates'
 
 
 def getDataBase():
-    return pymysql.connect(SERVER_ADDRESS, MYSQL_USER, MYSQL_PASS, MYSQL_DB, charset="utf8")
+    # return pymysql.connect(SERVER_ADDRESS, MYSQL_USER, MYSQL_PASS, MYSQL_DB, charset="utf8")
+    return pymysql.connect(SERVER_ADDRESS, MYSQL_USER, MYSQL_PASS, MYSQL_DB)
 
 
 @app.route('/chat/search', methods=['GET', 'POST'])
